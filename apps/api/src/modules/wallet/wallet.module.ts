58e9ba7ad07b9071c7720a10be9d38979b617553
pizza <pizza6899@gmail.com>
2025-12-29 12:22:11 +0700

    feat: Enhance authorization by implementing global CaslAbilityGuard, allowing unrestricted access for endpoints without @CheckAbility() decorator

---
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -0,0 +1,19 @@
+// src/modules/wallet/wallet.module.ts
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    // TODO: 필요한 모듈 import 추가
+  ],
+  providers: [
+    // TODO: Providers 추가
+  ],
+  controllers: [
+    // TODO: Controllers 추가
+  ],
+  exports: [
+    // TODO: Exports 추가
+  ],
+})
+export class WalletModule {}
+
