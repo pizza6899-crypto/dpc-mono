01abc025b52729280bf1041341a61893437f6089
pizza <pizza6899@gmail.com>
2025-12-31 19:33:31 +0700

    fix: Update error handling in API responses and improve validation logic for user input

---
+++ apps/api/src/modules/casino-refactor/casino-refactor.module.ts
@@ -0,0 +1,19 @@
+// src/modules/casino-refactor/casino-refactor.module.ts
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    // TODO: 필요한 모듈 import 추가
+  ],
+  providers: [
+    // TODO: Providers 추가
+  ],
+  controllers: [
+    // TODO: Controllers 추가
+  ],
+  exports: [
+    // TODO: Exports 추가
+  ],
+})
+export class CasinoRefactorModule {}
+
