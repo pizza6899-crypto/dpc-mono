b445ac79a6685f0cce769079761e94063cbc46f8
pizza <pizza6899@gmail.com>
2025-12-31 12:53:43 +0700

    remove: admin 프로젝트 삭제

--- apps/admin/src/app/forgot-password/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthPage } from "@components/auth-page";
-
-export default function ForgotPassword() {
-  return <AuthPage type="forgotPassword" />;
-}
--- apps/admin/src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { AuthPage } from "@components/auth-page";
-
-export default function Login() {
-  return <AuthPage type="login" />;
-}
