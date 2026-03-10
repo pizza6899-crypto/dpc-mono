644fcc73a2426d1cf06145399976a88ac703927c
pizza <pizza6899@gmail.com>
2026-03-10 18:31:14 +0700

    feat(chat): 가상 멤버 방식의 라운지 채팅 모듈 스캐폴딩 및 정책 구현

---
+++ apps/api/src/modules/chat/lounge/chat-lounge.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ChatRoomsModule } from '../rooms/chat-rooms.module';
+import { SnowflakeModule } from 'src/common/snowflake/snowflake.module';
+
+@Module({
+    imports: [
+        ChatRoomsModule, // Repository(ChatRoom, ChatMessage) 재사용을 위해 import
+        SnowflakeModule,
+    ],
+    controllers: [
+    ],
+    providers: [
+    ],
+    exports: [
+    ],
+})
+export class ChatLoungeModule { }
