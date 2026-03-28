187405919f5cd576cb053c6bdc72b17f41967e64
pizza <pizza6899@gmail.com>
2026-03-28 16:33:36 +0700

    chore: scaffold UniversalLogModule and remove unused success-response dto

--- apps/api/src/common/dtos/success-response.dto.ts
+++
@@ -1,7 +0,0 @@
-// src/common/dtos/success-response.dto.ts
-import { ApiProperty } from '@nestjs/swagger';
-
-export class SuccessResponseDto {
-  @ApiProperty({ description: 'Operation successful', example: true })
-  success: boolean;
-}
