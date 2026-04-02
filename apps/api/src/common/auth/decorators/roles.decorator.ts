2a76ac0e716a3027576f173d5a003b0012134005
pizza <pizza6899@gmail.com>
2026-04-02 23:10:33 +0700

    chore(auth): restore ApiCookieAuth in RequireRoles/Perms per convention

--- apps/api/src/common/auth/decorators/roles.decorator.ts
+++ apps/api/src/common/auth/decorators/roles.decorator.ts
@@ -1,4 +1,5 @@
 import { applyDecorators, SetMetadata } from '@nestjs/common';
+import { ApiCookieAuth } from '@nestjs/swagger';
 import { UserRoleType } from '@prisma/client';
 
 export const IS_PUBLIC_KEY = 'isPublic';
@@ -10,7 +11,7 @@ export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 export const GuestOnly = () => SetMetadata(GUEST_ONLY_KEY, true);
 
 export const RequireRoles = (...aud: Array<UserRoleType>) =>
-  applyDecorators(SetMetadata(ROLES_KEY, aud));
+  applyDecorators(SetMetadata(ROLES_KEY, aud), ApiCookieAuth());
 
 export const Perms = (...perms: string[]) =>
-  applyDecorators(SetMetadata(PERMISSIONS_KEY, perms));
+  applyDecorators(SetMetadata(PERMISSIONS_KEY, perms), ApiCookieAuth());
